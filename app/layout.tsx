926f615c3554647c45c404eef1414904bb1c8433
村田直人 <muratanaoto@muratanaotonoMacBook-Pro.local>
2025-03-09 05:07:49 +0900

    chapter10 metaタグ関連設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,25 @@
-import './globals.css';
-import Header from './_components/Header';
-import Footer from './_components/Footer';
+import "./globals.css";
+
+import type { Metadata } from "next";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://portfolio-nextjs-five-beige.vercel.app/"),
+  title: {
+    template: "%s | コーポレートサイト",
+    default: "コーポレートサイト",
+  },
+  description: "ディスクリプション",
+  openGraph: {
+    title: "コーポレートサイト",
+    description: "ディスクリプション",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "https://portfolio-nextjs-five-beige.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
